916cb394160c6dba5c7501a1d016ea395df870db
myusdem <myusdem@gmail.com>
2016-10-27 19:04:13 +0300

    Start

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { GraphhopperNgPage } from './app.po';
+import { browser } from 'protractor';
+import { protractor } from 'protractor';
+
+declare var $: any;
+
+describe('graphhopper-ng App', function() {
+  let page: GraphhopperNgPage;
+
+  beforeEach(() => {
+    page = new GraphhopperNgPage();
+    page.navigateTo();
+  });
+
+  it('should display message saying app works', () => {
+    expect(page.getParagraphText()).toEqual('Graphhopper NG');
+  });
+
+  it('should add and delete marker on map', () => {
+    page.addMarker();
+    page.clickOnMap(200, 200);
+    page.removeMarker();
+    page.clickOnMap(210, 210);
+    expect(page.countMarkers()).toBe(0);
+  });
+
+  it('should search for London', () => {
+    page.search("london");
+    browser.wait(protractor.ExpectedConditions.presenceOf($('#place-input[ng-reflect-model="London, UK"]')), 2000);    
+    expect(page.getLocation()).toEqual("London, UK");
+  });
+
+
+});
